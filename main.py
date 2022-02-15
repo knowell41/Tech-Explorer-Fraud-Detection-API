74a1e56aaabeaa872dc4af63e97c271e0d3b7195
Emmanuel Pagayonan <emmanuel_pagayonan_28@yahoo.com>
2022-02-16 00:57:23 +0800

    debugging bucketname detection

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from typing import Optional
 from fastapi import FastAPI
 
 import os
-import cloudstorage
-from google.appengine.api import app_identity
+# import cloudstorage
+# from google.appengine.api import app_identity
 
 app = FastAPI()
 
-BUCKET_NAME = os.environ.get('BUCKET_NAME', app_identity.get_default_gcs_bucket_name())
+# BUCKET_NAME = os.environ.get('BUCKET_NAME', app_identity.get_default_gcs_bucket_name())
 
 @app.get("/")
 def read_root():
-    return {"BUCKET_NAME": BUCKET_NAME}
+    context = {}
+    context["BUCKET_NAME"] = BUCKET_NAME
+    return context
 
