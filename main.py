4b4cf725129a464aaf6c1607fc90c0d59e3b36e3
Emmanuel Pagayonan <emmanuel_pagayonan_28@yahoo.com>
2022-02-18 01:45:27 +0800

    debugging app engine detect-fraud

--- main.py
+++ main.py
@@ -43,24 +43,24 @@ def read_root():
 
 @app.post("/detect-fraud")
 def detect_fraud(logs: UploadFile = File(...)):
-    try:
-        contentbyte = logs.file.read()
-        suffix_ = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
-        filename = f"upload_{suffix_}.csv"
-        with open(f"./static/uploads/{filename}", 'wb') as f:
-            f.write(contentbyte)
-        que_data = pd.read_csv(f"./static/uploads/{filename}")
-        X_, _ = preprocess_(que_data)
-        predicted_ = (fraud_detection_model.predict(X_)>0.5).astype("int32")
-        X_["prediction"] = predicted_
-        X_["prediction"] = X_["prediction"].apply(label_encoder)
-        X_["type"] = label.inverse_transform(X_["type"])
-        context = {}
-        context["status"] = True
-        context["data"] = X_.to_dict('records')
-    except Exception as e:
-        context = {}
-        context["status"] = False
-        context["Error"] = e
+    # try:
+    contentbyte = logs.file.read()
+    suffix_ = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+    filename = f"upload_{suffix_}.csv"
+    with open(f"./static/uploads/{filename}", 'wb') as f:
+        f.write(contentbyte)
+    que_data = pd.read_csv(f"./static/uploads/{filename}")
+    X_, _ = preprocess_(que_data)
+    predicted_ = (fraud_detection_model.predict(X_)>0.5).astype("int32")
+    X_["prediction"] = predicted_
+    X_["prediction"] = X_["prediction"].apply(label_encoder)
+    X_["type"] = label.inverse_transform(X_["type"])
+    context = {}
+    context["status"] = True
+    context["data"] = X_.to_dict('records')
+    # except Exception as e:
+    #     context = {}
+    #     context["status"] = False
+    #     context["Error"] = e
     
     return context
